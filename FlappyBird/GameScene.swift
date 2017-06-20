ff845b2324f1bfe707a1f0605f22c667286de935
chaixb <chaixiaobing7@gmail.com>
2017-06-20 22:47:18 +0800

    源码注释

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -32,7 +32,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         canRestart = false
         
         // setup physics
+        // 重力
         self.physicsWorld.gravity = CGVector( dx: 0.0, dy: -5.0 )
+      
+        /**
+         * contactDelegate是实现了SKPhysicsContactDelegate协议的对象，当两个物体接触时会作出反应，
+         * 要接受接触的消息，要将physicsWorld的contactDelegate
+         * 接触开始或结束时，代理会被执行
+         */
         self.physicsWorld.contactDelegate = self
         
         // setup background color
@@ -46,22 +53,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
         // ground
         let groundTexture = SKTexture(imageNamed: "land")
-        groundTexture.filteringMode = .nearest // shorter form for SKTextureFilteringMode.Nearest
-        
-        let moveGroundSprite = SKAction.moveBy(x: -groundTexture.size().width * 2.0, y: 0, duration: TimeInterval(0.02 * groundTexture.size().width * 2.0))
-        let resetGroundSprite = SKAction.moveBy(x: groundTexture.size().width * 2.0, y: 0, duration: 0.0)
+      
+        /**
+         * filteringMode指明当图片不够大将他缩放时按照什么模式处理
+         * .nearest: where each pixel is drawn using the nearest point in the texture
+         * .linear: where each pixel is drawn by using a linear filter of multiple texels in the texture
+         */
+        groundTexture.filteringMode = .linear // shorter form for SKTextureFilteringMode.Nearest
+      
+        let groundWidth = groundTexture.size().width
+        // 下面所有的代码乘以2是因为使用的是最小比例的图片？？
+        // 大地的运动
+        let moveGroundSprite = SKAction.moveBy(x: -groundWidth * 2.0, y: 0, duration: TimeInterval(0.02 * groundWidth * 2.0))
+        // 重置大地的位置
+        let resetGroundSprite = SKAction.moveBy(x: groundWidth * 2.0, y: 0, duration: 0.0)
+        // 无限移动大地，移动到最左侧时回到最右侧
         let moveGroundSpritesForever = SKAction.repeatForever(SKAction.sequence([moveGroundSprite,resetGroundSprite]))
         
-        for i in 0 ..< 2 + Int(self.frame.size.width / ( groundTexture.size().width * 2 )) {
+        // 所看到的下面的土地是由很多小块的拼接起来的，注意数量加了2，形成了连续性
+        for i in 0 ..< 2 + Int(self.frame.size.width / ( groundWidth * 2 )) {
             let i = CGFloat(i)
             let sprite = SKSpriteNode(texture: groundTexture)
             sprite.setScale(2.0)
+            // 设置位置
             sprite.position = CGPoint(x: i * sprite.size.width, y: sprite.size.height / 2.0)
+            // 每个节点独立运动
             sprite.run(moveGroundSpritesForever)
+            // 添加到父节点
             moving.addChild(sprite)
         }
         
-        // skyline
+        // 以下同理
         let skyTexture = SKTexture(imageNamed: "sky")
         skyTexture.filteringMode = .nearest
         
@@ -86,8 +108,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         pipeTextureDown.filteringMode = .nearest
         
         // create the pipes movement actions
+        // 管道要移动的距离等于屏幕宽度加上自身宽度
         let distanceToMove = CGFloat(self.frame.size.width + 2.0 * pipeTextureUp.size().width)
         let movePipes = SKAction.moveBy(x: -distanceToMove, y:0.0, duration:TimeInterval(0.01 * distanceToMove))
+        
+        // 管道移除
         let removePipes = SKAction.removeFromParent()
         movePipesAndRemove = SKAction.sequence([movePipes, removePipes])
         
@@ -142,15 +167,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     }
     
     func spawnPipes() {
+        // 上下成对出现的管道
         let pipePair = SKNode()
+        
+        // 初始位置在右侧屏幕的外侧，向左运动
         pipePair.position = CGPoint( x: self.frame.size.width + pipeTextureUp.size().width * 2, y: 0 )
         pipePair.zPosition = -10
         
         let height = UInt32( self.frame.size.height / 4)
-        let y = Double(arc4random_uniform(height) + height);
+        // Y轴的位置随机
+        let y = Double(arc4random_uniform(height) + height)
         
         let pipeDown = SKSpriteNode(texture: pipeTextureDown)
         pipeDown.setScale(2.0)
+        
+        // 管道口之间的距离固定，计算两个Y轴便宜
         pipeDown.position = CGPoint(x: 0.0, y: y + Double(pipeDown.size.height) + verticalPipeGap)
         
         
@@ -171,10 +202,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         pipePair.addChild(pipeUp)
         
         let contactNode = SKNode()
+        
+        // 判断得分的点
         contactNode.position = CGPoint( x: pipeDown.size.width + bird.size.width / 2, y: self.frame.midY )
         contactNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize( width: pipeUp.size.width, height: self.frame.size.height ))
         contactNode.physicsBody?.isDynamic = false
         contactNode.physicsBody?.categoryBitMask = scoreCategory
+        
+        // 如果小鸟通过了这个检测点，将得分
         contactNode.physicsBody?.contactTestBitMask = birdCategory
         pipePair.addChild(contactNode)
         
@@ -228,6 +263,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
                 score += 1
                 scoreLabelNode.text = String(score)
                 
+                // 分数增加的一点小特效
                 // Add a little visual feedback for the score increment
                 scoreLabelNode.run(SKAction.sequence([SKAction.scale(to: 1.5, duration:TimeInterval(0.1)), SKAction.scale(to: 1.0, duration:TimeInterval(0.1))]))
             } else {
